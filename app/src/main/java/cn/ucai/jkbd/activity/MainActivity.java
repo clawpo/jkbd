795f509bcf1bdb50178b079bf753c28b6ab46030
耿小泰 <clawpo@foxmail.com>
2017-06-29 10:23:45 +0800

    把activity放到对应的包里

--- app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package cn.ucai.jkbd;
+package cn.ucai.jkbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import cn.ucai.jkbd.R;
 
 public class MainActivity extends AppCompatActivity {
 
