7bd8ad3c628220c407e602f5cb65fa7757879672
耿小泰 <clawpo@foxmail.com>
2017-06-30 11:01:56 +0800

    下载考试题并显示

--- app/src/main/java/cn/ucai/jkbd/ExamApplication.java
+++ app/src/main/java/cn/ucai/jkbd/ExamApplication.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import cn.ucai.jkbd.bean.Exam;
 import cn.ucai.jkbd.bean.ExamInfo;
+import cn.ucai.jkbd.bean.Result;
 import cn.ucai.jkbd.utils.OkHttpUtils;
+import cn.ucai.jkbd.utils.ResultUtils;
 
 /**
  * Created by clawpo on 2017/6/30.
@@ -31,22 +33,50 @@ public class ExamApplication extends Application {
     }
 
     private void initData() {
-        OkHttpUtils<ExamInfo> utils = new OkHttpUtils<>(instance);
-        String uri = "http://101.251.196.90:8080/JztkServer/examInfo";
-        utils.url(uri)
-                .targetClass(ExamInfo.class)
-                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
-                    @Override
-                    public void onSuccess(ExamInfo result) {
-                        Log.e("main","result="+result);
-                        mExamInfo = result;
-                    }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<ExamInfo> utils = new OkHttpUtils<>(instance);
+                String uri = "http://101.251.196.90:8080/JztkServer/examInfo";
+                utils.url(uri)
+                        .targetClass(ExamInfo.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
+                            @Override
+                            public void onSuccess(ExamInfo result) {
+                                Log.e("main","result="+result);
+                                mExamInfo = result;
+                            }
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","error="+error);
-                    }
-                });
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+
+
+                OkHttpUtils<String> utils1 = new OkHttpUtils<>(instance);
+                String url2 = "http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>() {
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                Result result = ResultUtils.getListResultFromJson(jsonStr);
+                                if (result!=null && result.getError_code()==0){
+                                    List<Exam> list = result.getResult();
+                                    if (list!=null && list.size()>0){
+                                        mExamList = list;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+            }
+        }).start();
     }
 
     public ExamInfo getExamInfo() {
--- app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/ExamActivity.java
@@ -3,10 +3,14 @@ package cn.ucai.jkbd.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import cn.ucai.jkbd.ExamApplication;
 import cn.ucai.jkbd.R;
+import cn.ucai.jkbd.bean.Exam;
 import cn.ucai.jkbd.bean.ExamInfo;
 
 /**
@@ -14,7 +18,8 @@ import cn.ucai.jkbd.bean.ExamInfo;
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tvOp1,tvOp2,tvOp3,tvOp4;
+    ImageView mImageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +30,33 @@ public class ExamActivity extends AppCompatActivity {
 
     private void initView() {
         tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+        tvExamTitle = (TextView) findViewById(R.id.tv_exam_title);
+        tvOp1 = (TextView) findViewById(R.id.tv_op1);
+        tvOp2 = (TextView) findViewById(R.id.tv_op2);
+        tvOp3 = (TextView) findViewById(R.id.tv_op3);
+        tvOp4 = (TextView) findViewById(R.id.tv_op4);
+        mImageView = (ImageView) findViewById(R.id.im_exam_image);
     }
 
     private void initData() {
         ExamInfo examInfo = ExamApplication.getInstance().getExamInfo();
         if (examInfo!=null){
             showData(examInfo);
+        }
+        List<Exam> examList = ExamApplication.getInstance().getExamList();
+        if (examList!=null){
+            showExam(examList);
+        }
+    }
+
+    private void showExam(List<Exam> examList) {
+        Exam exam = examList.get(0);
+        if (exam!=null){
+            tvExamTitle.setText(exam.getQuestion());
+            tvOp1.setText(exam.getItem1());
+            tvOp2.setText(exam.getItem2());
+            tvOp3.setText(exam.getItem3());
+            tvOp4.setText(exam.getItem4());
         }
     }
 
